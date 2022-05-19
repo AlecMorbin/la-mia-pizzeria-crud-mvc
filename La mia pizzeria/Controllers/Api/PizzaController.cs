7e710066fb17adbdd373d800f9ab321ebb60434d
Alec <alemor98@hotmail.it>
2022-05-19 15:59:05 +0200

    Implementato GET con API tramite Axuis

---
+++ La mia pizzeria/Controllers/Api/PizzaController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using La_mia_pizzeria.Models;
+using La_mia_pizzeria.Data;
+
+namespace La_mia_pizzeria.Controllers.Api
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PizzaController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<Pizza> listPizzas = new List<Pizza>();
+
+            using (PizzaContext db = new PizzaContext()) 
+            {
+                listPizzas = db.Pizzas.ToList<Pizza>();
+            }
+
+            return Ok(listPizzas);
+        }
+
+    }
+}
